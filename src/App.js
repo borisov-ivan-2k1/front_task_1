d1af1b7ca2c41e1893b53d36278a87f28b59240b
Ivan <iborisov@pryaniky.onmicrosoft.com>
2020-08-15 13:50:09 +0300

    good

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { YMaps, Map, Placemark } from 'react-yandex-maps';
+
 function App() {
+
+  const [ placeMarks, setPlaceMarks ] = useState([])
+
+  const [ toolbar, setToolbar ] = useState(false)
+
+  const mapData = {
+    center: [55.751574, 37.573856],
+    zoom: 10
+  }
+
+  const onAddItem = (e) => {
+    const name = prompt('Название метки');
+    setPlaceMarks([...placeMarks, {coords: e.get('coords'), name}])
+  }
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <YMaps>
+          <Map defaultState={mapData} width='100vw' height='100vh' onClick={(e) => onAddItem(e)}>
+            {placeMarks.map(placeMark => (
+              <Placemark geometry={placeMark.coords} properties={{iconContent: placeMark.name}}/>
+            ))}
+            
+          </Map>
+        </YMaps>
+      </div>
+
+      <div className={`toolBar ${toolbar ? 'open' : 'close'}`} onClick={() => setToolbar(!toolbar)}>
+
+      </div>
     </div>
   );
 }
